638ad52180de24e99106cf461976147cf6885985
jocelynjoel <joel.jocelyn@hotmail.fr>
2022-03-17 14:47:49 -0700

    uppercase script

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * _isupper - check uppercase letter
+ * @c: char variable
+ * Return: 1 or 0
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
