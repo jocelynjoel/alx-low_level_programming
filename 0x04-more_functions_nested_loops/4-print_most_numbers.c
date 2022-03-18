0028398a5425762bf71af2d9a098ea426cc1dfce
jocelynjoel <joel.jocelyn@hotmail.fr>
2022-03-17 17:11:41 -0700

    printing script

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * print_most_numbers - printing number without 2 and 4
+ * Return: nothing
+ */
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}
