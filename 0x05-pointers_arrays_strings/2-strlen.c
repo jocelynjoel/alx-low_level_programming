70baa8000cab102a546fb039b3c4d5a473964455
jocelynjoel <joel.jocelyn@hotmail.fr>
2022-03-21 13:04:15 -0700

    task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include<string.h>
+#include"main.h"
+
+/**
+ * _strlen - funtion that return length of char
+ * @s: char variable
+ *
+ * Return: nothing
+ */
+int _strlen(char *s)
+{
+	strlen (*s);
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,2 +1,3 @@
 task0  a function that takes a pointer to an int as parameter and updates the value it points to to 98.
 task1 swaps the values of two integers.
+task 2  returns the length of a string.
