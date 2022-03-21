152f0bdb0fdc24909056a3554f0b1dbc16cc71a4
jocelynjoel <joel.jocelyn@hotmail.fr>
2022-03-21 15:35:38 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include"main.h"
+
+/**
+ * print_rev - function that print string reverse
+ * @str: string value
+ *
+ * Return: nothing
+ */
+void print_rev(char *str)
+{
+	int i = 0;
+
+	while (str != '\0')
+	{
+		s++;
+		i++;
+	}
+	while (i > 0)
+	{
+		s--;
+		_putchar(*s);
+		i--;
+	}
+	_putchar('\n')
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -2,3 +2,4 @@ task0  a function that takes a pointer to an int as parameter and updates the va
 task1 swaps the values of two integers.
 task 2  returns the length of a string.
 task 3 prints a string, followed by a new line, to stdout.
+task 4 prints a string, in reverse, followed by a new line.
