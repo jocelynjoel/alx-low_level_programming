fced8e15ab1e59f27217458427703174a388cff7
jocelynjoel <joel.jocely@hotmail.fr>
2022-04-01 18:31:31 +0000

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - function that print the number of argument
+ * @argc: first argument
+ * @argv: second argument
+ * Return: 0
+ */
+int main(int argc, char *argv __attribute__((unused)))
+{
+	print("%d\n", argc);
+	return(0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,3 +1,4 @@
 # 0x0A. C - argc, argv tasks
 
 task 0: prints its name, followed by a new line.
+task 1: prints the number of arguments passed into it.
