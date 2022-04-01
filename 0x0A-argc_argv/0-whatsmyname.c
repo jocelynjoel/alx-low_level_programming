344740b83124a8046256baee3fe862ce296aa93c
jocelynjoel <joel.jocely@hotmail.fr>
2022-04-01 18:19:25 +0000

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - function for name printing
+ * @argc:  counting argument
+ * @argv: array of char
+ * Return: 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+# 0x0A. C - argc, argv tasks
+
+task 0: prints its name, followed by a new line.
