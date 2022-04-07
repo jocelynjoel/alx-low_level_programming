66f770415bb9f9d631648590ca47bbb7cd668ec7
jocelynjoel <joel.jocely@hotmail.fr>
2022-04-07 21:41:48 +0000

    fix task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,41 +1,57 @@
 #include "main.h"
+
 /**
- * _realloc - Reallocates a space in memory
- * @ptr: The pointer with allocated size in memory
- * @old_size: Current allocated size
- * @new_size: The new size to realloc
- * Return: Nothing
+ * _memcpy - copy memory data from src to dest
+ * @dest: memory destination
+ * @src: memory source
+ * @n: size of new memory
  */
+void _memcpy(void *dest, void *src, size_t n)
+{
+	size_t i;
+	char *csrc = (char *)src;
+	char *cdest = (char *)dest;
+
+	for (i = 0; i < n; i++)
+		cdest[i] = csrc[i];
+}
+
+/**
+ * *_realloc - reallocates a memory block using malloc and free
+ * @ptr: array length
+ * @old_size: size of old memory
+ * @new_size: size of new memory
+ * Return: pointer to new memory
+ */
+
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *str, *pptr;
-	unsigned int i = 0;
 
-	if (new_size == old_size)
-		return (ptr);
-	if (ptr == NULL)
-	{
-		str = malloc(new_size);
-		if (str == NULL)
-			return (NULL);
-		return (str);
-	}
-	if (new_size == 0 && ptr != NULL)
-	{
+void *newPtr;
+if (new_size == 0)
+{
+	if (ptr != NULL)
 		free(ptr);
-		return (NULL);
-	}
-	str = malloc(new_size);
-	if (str == 0)
-		return (NULL);
-	pptr = ptr;
-
-	while (i < old_size)
+	return (NULL);
+}
+else if (!ptr)
+{
+	return (malloc(new_size));
+}
+else if (new_size <= old_size)
+{
+	return (ptr);
+}
+else
+{
+	newPtr = malloc(new_size);
+	if (newPtr)
 	{
-		str[i] = pptr[i];
-		i++;
+		_memcpy(newPtr, ptr, old_size);
+		free(ptr);
 	}
+	return (newPtr);
+}
 
-	free(ptr);
-	return (str);
+return (0);
 }
