01316f797684ff915a7a4efafee1f00bf930e772
jocelynjoel <joel.jocely@hotmail.fr>
2022-04-28 23:28:50 +0000

    fix task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,74 +1,37 @@
 #include "main.h"
-#include <stdio.h>
-/**
-  *_strlen - returns the length of a string.
-  *@s: pointer to string.
-  *
-  *Return: length.
-  */
-unsigned int _strlen(const char *s)
-{
-	unsigned int i;
-
-	i = 0;
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
 
 /**
-  *getp - returns the value of x to the power of y.
-  *@x: number.
-  *@y: power.
-  *
-  *Return: x to the pow of y.
-  *0 if x < 0.
-  */
-int getp(int x, int y)
-{
-	if (y < 0)
-		return (0);
-	if (y == 0)
-		return (1);
-	if (y == 1)
-		return (x);
-
-	return (x * getp(x, y - 1));
-}
-
-/**
-  *binary_to_uint - converts a binary number to an unsigned int.
-  *@b: pointer to string containing 0 and 1.
-  *
-  *Return: converted number or 0 if b is null or has chars not 0 or 1.
-  */
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
+ *
+ * Return: unsigned int.
+ */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num, length, index;
-	int power;
+	unsigned int ui;
+	int len, base_two;
 
 	if (!b)
 		return (0);
 
-	length = _strlen(b);
-	power = 0;
-	num = 0;
-	index = length - 1;
-	while (length > 0)
+	ui = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		if (b[index] == 48 || b[index] == 49)
-		{
-			num = num + ((b[index] - 48) * getp(2, power));
-			power++;
-			index--;
-			length--;
-		}
-		else
+		if (b[len] != '0' && b[len] != '1')
 		{
 			return (0);
 		}
+
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
 	}
-	return (num);
+
+	return (ui);
 }
